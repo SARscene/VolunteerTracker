6c476e545de2af616e0fb4a46d4e1f7e33434e61
Ron Myers <ron@thinkingbig.net>
2015-10-23 20:18:21 -0300

    Initial app bits to build on.

---
+++ client/www/js/controllers.js
@@ -0,0 +1,15 @@
+app.controller('StartCtrl', function($scope) {
+  console.log('starting app');
+
+  $scope.goToEntry = function()
+  {
+    console.log('starting');
+  };
+})
+
+
+.controller('EntryCtrl', function($scope){
+
+});
+
+
