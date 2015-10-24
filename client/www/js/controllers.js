96df2cebd36ea391a44d12e792cf372300e91074
Ron Myers <ron@thinkingbig.net>
2015-10-24 09:28:24 -0300

    Moved to our own interval vs watchPosition

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -21,7 +21,7 @@ app.controller('StartCtrl', function($scope, $state)
   };
 })
 
-.controller('TrackingCtrl', function($scope, $state){
+.controller('TrackingCtrl', function($scope, $state, $interval){
   var watchID = null;
   $scope.points = [];
   $scope.lastPoint = "loading first point...";
@@ -30,38 +30,38 @@ app.controller('StartCtrl', function($scope, $state)
   {
     console.log('tracking now');
 
+    var options = {
+      enableHighAccuracy: true,
+      frequency: 100
+    };
+
     try
     {
-      // setup gps watch
-      watchID = navigator.geolocation.watchPosition(function(position){
-          console.log('point received');
-          console.dir(position);
-          $scope.lastPoint = {
-             latitude:          position.coords.latitude,
-             longitude:         position.coords.longitude,
-             altitude:          position.coords.altitude,
-             accuracy:          position.coords.accuracy,
-             altitudeAccuracy:  position.coords.altitudeAccuracy,
-             heading:           position.coords.heading,
-             speed:             position.coords.speed,
-             timestamp:         position.timestamp
-           };
-          $scope.points.push($scope.lastPoint);
-
-          $scope.$apply();
-        },
-        function(err){
-          console.log('failed to get point with error: ' + err.message);
-          alert("Error locating GPS coordinates with error: " + err.message);
-        },
-        { maximumAge: 10, timeout: 10, enableHighAccuracy: false });
-
+      watchID = $interval(function() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log('point received');
+            console.dir(position);
+            $scope.lastPoint = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              altitude: position.coords.altitude,
+              accuracy: position.coords.accuracy,
+              altitudeAccuracy: position.coords.altitudeAccuracy,
+              heading: position.coords.heading,
+              speed: position.coords.speed,
+              timestamp: position.timestamp
+            };
+            $scope.points.push($scope.lastPoint);
+          }, function (err) {
+            console.log('failed to get point with error: ' + err.message);
+          }
+          , options);
+      });
     }
     catch(err)
     {
       var msg = 'Error thrown setting up GPS watch: '+ err.message;
       console.log(msg);
-      alert(msg);
     }
   };
 
@@ -70,5 +70,6 @@ app.controller('StartCtrl', function($scope, $state)
   $scope.endTracking = function()
   {
     console.log('end tracking');
+    $interval.cancel(watchID);
   };
 });
