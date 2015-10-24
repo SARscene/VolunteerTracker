e7ee8f97fbc2d61926d4e0fa9b95d56459a02861
Ron Myers <ron@thinkingbig.net>
2015-10-24 07:35:14 -0300

    First pass at tracking points - additional debug code added to help with iOS debugging (note alerts need to come out)

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -23,27 +23,46 @@ app.controller('StartCtrl', function($scope, $state)
 
 .controller('TrackingCtrl', function($scope, $state){
   var watchID = null;
+  $scope.points = [];
   $scope.lastPoint = "loading first point...";
 
   $scope.beingTracking = function()
   {
     console.log('tracking now');
 
-    watchID = navigator.geolocation.watchPosition(function(position){
-        console.log('point received');
-        console.dir(position);
-        $scope.lastPoint = {
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-          timestamp: position.timestamp
-        };
-        $scope.$apply();
-      },
-      function(){
-        console.log('');
-      },
-      { maximumAge: 1000, timeout: 1000, enableHighAccuracy: true });
+    try
+    {
+      // setup gps watch
+      watchID = navigator.geolocation.watchPosition(function(position){
+          console.log('point received');
+          console.dir(position);
+          $scope.lastPoint = {
+             latitude:          position.coords.latitude,
+             longitude:         position.coords.longitude,
+             altitude:          position.coords.altitude,
+             accuracy:          position.coords.accuracy,
+             altitudeAccuracy:  position.coords.altitudeAccuracy,
+             heading:           position.coords.heading,
+             speed:             position.coords.speed,
+             timestamp:         position.timestamp
+           };
+          $scope.points.push($scope.lastPoint);
 
+          $scope.$apply();
+        },
+        function(err){
+          console.log('failed to get point with error: ' + err.message);
+          alert("Error locating GPS coordinates with error: " + err.message);
+        },
+        { maximumAge: 10, timeout: 10, enableHighAccuracy: false });
+
+    }
+    catch(err)
+    {
+      var msg = 'Error thrown setting up GPS watch: '+ err.message;
+      console.log(msg);
+      alert(msg);
+    }
   };
 
   $scope.beingTracking();
