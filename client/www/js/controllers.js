f0f1fb94fc46159e4f9248d0990e3f60827267a3
Ron Myers <ron@thinkingbig.net>
2015-10-23 21:27:48 -0300

    Added GPS and outputting initial values.

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -22,8 +22,34 @@ app.controller('StartCtrl', function($scope, $state)
 })
 
 .controller('TrackingCtrl', function($scope, $state){
+  var watchID = null;
+  $scope.lastPoint = "loading first point...";
+
   $scope.beingTracking = function()
   {
     console.log('tracking now');
+
+    watchID = navigator.geolocation.watchPosition(function(position){
+        console.log('point received');
+        console.dir(position);
+        $scope.lastPoint = {
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+          timestamp: position.timestamp
+        };
+        $scope.$apply();
+      },
+      function(){
+        console.log('');
+      },
+      { maximumAge: 1000, timeout: 1000, enableHighAccuracy: true });
+
+  };
+
+  $scope.beingTracking();
+
+  $scope.endTracking = function()
+  {
+    console.log('end tracking');
   };
 });
